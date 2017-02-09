0fe0bcaa3f090c1a428774b475310ce510095bd6
Bojie Jiang <jiangbojie@gmail.com>
2017-02-08 21:28:21 -0800

    First commit

---
+++ app/src/main/java/us/bojie/listwidget/WidgetService.java
@@ -0,0 +1,11 @@
+package us.bojie.listwidget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new WidgetAdapter(this);
+    }
+}
